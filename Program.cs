4c06946de98b45c01067c9310ce2c94cd9843f01
Mike Tamis <tamis.mike@gmail.com>
2014-05-25 15:13:27 +1200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Kevin.Core;
+
+namespace Kevin.Console
+{
+	class MainClass : Output
+	{
+		static KevinCore core;
+
+		public static void Main (string[] args)
+		{
+			MainClass instance = new MainClass ();
+			core = new KevinCore (instance);
+
+			if (args.Length == 0) { 
+				instance.send ("Hello.");
+				instance.Loop ();
+			}
+
+			core.tell(string.Join(" ", args));
+		}
+
+		public void Loop ()
+		{
+			while (true) {
+				System.Console.Write ("You: ");
+				string input = System.Console.ReadLine ();
+				core.tell (input);
+			}
+
+		}
+
+		#region Output implementation
+
+		public void send (string hey)
+		{	
+			System.Console.WriteLine ("Kevin: " + hey);
+		}
+
+		#endregion
+	}
+}
